258068411d4e970d7f60d287475a258fe2f2f75f
hamed.bayat <hamed.bayat@snapptrip.com>
2020-07-28 00:33:21 +0430

    implement insert in dynamic array

--- src/com/hamed/Array.java
+++ src/com/hamed/Array.java
@@ -11,4 +11,23 @@ public class Array<T> {
     private T[] initialItems(int length) {
         return (T[]) new Object[length];
     }
+
+    public void insert(T item) {
+        if (isFull())
+            resize();
+
+        items[count++] = item;
+    }
+
+    private void resize() {
+        var newItems = initialItems(count * 2);
+        for (int i = 0; i < count; i++)
+            newItems[i] = items[i];
+        items = newItems;
+    }
+
+    private boolean isFull() {
+        return count == items.length;
+    }
+
 }
